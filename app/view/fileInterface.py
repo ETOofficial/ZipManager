f5d3672085903b7b952281f0fa0f3697d91347eb
ETO official <151742029+ETOofficial@users.noreply.github.com>
2025-02-15 22:57:44 +0800

    refactor(app): 重构文件接口视图
    - 移除了未使用的上下文菜单代码
    - 优化了 CustomTableWidget 类的鼠标事件处理逻辑
    - 添加了新的上下文菜单方法
    - 简化了 FileInterface 类的布局结构
    - 更新了下拉菜单选项

--- app/view/fileInterface.py
+++ app/view/fileInterface.py
@@ -4,75 +4,26 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QVBoxLayout, QTableWidgetItem, QFileDialog, QAction, QAbstractItemView
 from qfluentwidgets import ScrollArea, FluentIcon, CommandBar, Action, TableWidget, RoundMenu, MenuAnimationType, \
-    TransparentDropDownPushButton, setFont
+    TransparentDropDownPushButton, setFont, SmoothScrollArea
 
 from ..utils.fileinfo import getinfo, remove_nested
-
-    # def contextMenuEvent(self, e):
-    #     menu = RoundMenu(parent=self)
-    #     # menu = CheckableMenu(parent=self, indicatorType=MenuIndicatorType.RADIO)
-    # 
-    #     # NOTE: hide the shortcut key
-    #     # menu.view.setItemDelegate(MenuItemDelegate())
-    # 
-    #     # add actions
-    #     menu.addAction(Action(FluentIcon.COPY, 'Copy'))
-    #     menu.addAction(Action(FluentIcon.CUT, 'Cut'))
-    #     menu.actions()[0].setCheckable(True)
-    #     menu.actions()[0].setChecked(True)
-    # 
-    #     # add sub menu
-    #     submenu = RoundMenu("Add to", self)
-    #     submenu.setIcon(FluentIcon.ADD)
-    #     submenu.addActions([
-    #         Action(FluentIcon.VIDEO, 'Video'),
-    #         Action(FluentIcon.MUSIC, 'Music'),
-    #     ])
-    #     menu.addMenu(submenu)
-    # 
-    #     # add actions
-    #     menu.addActions([
-    #         Action(FluentIcon.PASTE, 'Paste'),
-    #         Action(FluentIcon.CANCEL, 'Undo')
-    #     ])
-    # 
-    #     # add separator
-    #     menu.addSeparator()
-    #     menu.addAction(QAction(f'Select all', shortcut='Ctrl+A'))
-    # 
-    #     # insert actions
-    #     menu.insertAction(
-    #         menu.actions()[-1], Action(FluentIcon.SETTING, 'Settings', shortcut='Ctrl+S'))
-    #     menu.insertActions(
-    #         menu.actions()[-1],
-    #         [Action(FluentIcon.HELP, 'Help', shortcut='Ctrl+H'),
-    #          Action(FluentIcon.FEEDBACK, 'Feedback', shortcut='Ctrl+F')]
-    #     )
-    #     menu.actions()[-2].setCheckable(True)
-    #     menu.actions()[-2].setChecked(True)
-    # 
-    #     # show menu
-    #     menu.exec(e.globalPos(), aniType=MenuAnimationType.DROP_DOWN)
 
 class CustomTableWidget(TableWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
     def mousePressEvent(self, event):
+        index = self.indexAt(event.pos())
+        row = index.row()
+        column = index.column()
         if event.button() == Qt.RightButton:
             # 获取点击的单元格位置
-            index = self.indexAt(event.pos())
             if index.isValid():
-                row = index.row()
-                column = index.column()
                 print(f"Right-click on Row {row}, Column {column}")
-                # # 显示上下文菜单
-                # self.showContextMenu(event.globalPos(), row, column)
+                # 显示上下文菜单
+                self.contextMenu(event, row, column)
         elif event.button() == Qt.LeftButton:
             # 处理左键点击事件
-            index = self.indexAt(event.pos())
-            row = index.row()
-            column = index.column()
             if index.isValid():
                 # 检查 Ctrl 键是否被按下
                 if event.modifiers() & Qt.ControlModifier:
@@ -95,7 +46,6 @@ class CustomTableWidget(TableWidget):
             # 检查 Ctrl 键是否被按下
             if event.modifiers() & Qt.ControlModifier:
                 print(f"Ctrl + Double-click on Row {row}, Column {column}")
-                # self.parent().add_select_path(path)
                 # 弹出确认窗口：打开所有选中的文件
             else:
                 print(f"Double-click on Row {row}, Column {column}")
@@ -127,6 +77,17 @@ class CustomTableWidget(TableWidget):
     #         if row not in select_rows:
     #             self.setRangeSelected(, True)
 
+    def contextMenu(self, event, row, column):
+        menu = RoundMenu(parent=self)
+
+        menu.addActions([
+            Action(FluentIcon.PEOPLE, '管理账户和设置'),
+            Action(FluentIcon.SHOPPING_CART, '支付方式'),
+            Action(FluentIcon.CODE, '兑换代码和礼品卡'),
+        ])
+
+        menu.exec(event.globalPos())
+
 class FileInterface(ScrollArea):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -137,7 +98,6 @@ class FileInterface(ScrollArea):
 
         # 创建布局实例
         self.vBoxLayout = QVBoxLayout(self)
-        self.vBoxLayout2 = QVBoxLayout(self)
         self.commandBar = CommandBar(self)
         self.dropDownButton = self.createDropDownButton()
 
@@ -178,7 +138,6 @@ class FileInterface(ScrollArea):
         # 将元素加入布局
         self.vBoxLayout.addWidget(self.commandBar)
         self.vBoxLayout.addWidget(self.tableView)
-
         
         print(f"{self.object_name} has been inited")
 
@@ -190,16 +149,15 @@ class FileInterface(ScrollArea):
 
     def createDropDownButton(self):
         button = TransparentDropDownPushButton('其它选项', self, FluentIcon.MORE)
-        button.setFixedHeight(34)
-        setFont(button, 12)
+        # button.setFixedHeight(34)
+        # setFont(button, 12)
 
         menu = RoundMenu(parent=self)
         menu.addActions([
-            Action(FluentIcon.COPY, 'Copy'),
-            Action(FluentIcon.CUT, 'Cut'),
-            Action(FluentIcon.PASTE, 'Paste'),
-            Action(FluentIcon.CANCEL, 'Cancel'),
-            Action('Select all'),
+            Action(FluentIcon.PLAY, '单独压缩每个文件'),
+            Action(FluentIcon.PLAY, '压缩选中文件'),
+            Action(FluentIcon.PLAY, '单独压缩每个选中文件'),
+            Action(FluentIcon.PLAY, '解压选中文件'),
         ])
         button.setMenu(menu)
         return button       
